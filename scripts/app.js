462b2f813ffe900cd97019813e7f29ee5e8789f3
ukandieSunday <ukandieudo202@gmail.com>
2023-09-30 17:36:46 +0100

    add files and also populate some html and css

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+//  const underline = document.querySelector(".underline").computedStyleMap.
+// setInterval(()=>{
+//   underline.classList.toggle('underline'); 
+//   underline.style.width = "100%"
+// },50)
+// const about = document.querySelector(".about-row");
+// const links = document.querySelectorAll(".links");
+//  const contents = document.querySelectorAll(".contents");
+// about.addEventListener("click",(event)=>{
+// const id = event.target.dataset.id;
+//   if(id){
+//     links.forEach((link)=>{
+//       link.classList.remove("active-link")
+//     })
+//     event.target.classList.add("active-link")
+//   }
+//   contents.forEach((content)=>{
+// content.classList.remove("active-content");
+//   })
+//   document.getElementById(id).classList.add("active-content");
+// })
