f1cdd4ff8b4e6db012befaea11a03db9c632a6a9
ukandieSunday <ukandieudo202@gmail.com>
2023-09-30 18:16:08 +0100

    add about functionality

--- scripts/app.js
+++ scripts/app.js
@@ -3,19 +3,19 @@
 //   underline.classList.toggle('underline'); 
 //   underline.style.width = "100%"
 // },50)
-// const about = document.querySelector(".about-row");
-// const links = document.querySelectorAll(".links");
-//  const contents = document.querySelectorAll(".contents");
-// about.addEventListener("click",(event)=>{
-// const id = event.target.dataset.id;
-//   if(id){
-//     links.forEach((link)=>{
-//       link.classList.remove("active-link")
-//     })
-//     event.target.classList.add("active-link")
-//   }
-//   contents.forEach((content)=>{
-// content.classList.remove("active-content");
-//   })
-//   document.getElementById(id).classList.add("active-content");
-// })
+const about = document.querySelector(".about-row");
+const links = document.querySelectorAll(".links");
+ const contents = document.querySelectorAll(".contents");
+about.addEventListener("click",(event)=>{
+const id = event.target.dataset.id;
+  if(id){
+    links.forEach((link)=>{
+      link.classList.remove("active-link")
+    })
+    event.target.classList.add("active-link")
+  }
+  contents.forEach((content)=>{
+content.classList.remove("active-content");
+  })
+  document.getElementById(id).classList.add("active-content");
+})
