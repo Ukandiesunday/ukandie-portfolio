7f44362087e700a5c42acd87462e7f0357f3689a
ukandieSunday <ukandieudo202@gmail.com>
2023-10-02 08:24:07 +0100

    add contact page

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,21 @@
-//  const underline = document.querySelector(".underline").computedStyleMap.
-// setInterval(()=>{
-//   underline.classList.toggle('underline'); 
-//   underline.style.width = "100%"
-// },50)
-const about = document.querySelector(".about-row");
+// To toggle 'about' links and contents based on matching id
+const about = document.querySelector(".js-about");
 const links = document.querySelectorAll(".links");
- const contents = document.querySelectorAll(".contents");
+const contents = document.querySelectorAll(".contents");
 about.addEventListener("click",(event)=>{
 const id = event.target.dataset.id;
   if(id){
     links.forEach((link)=>{
-      link.classList.remove("active-link")
+      link.classList.remove("active-link");
     })
-    event.target.classList.add("active-link")
-  }
+    event.target.classList.add("active-link");
+  
   contents.forEach((content)=>{
-content.classList.remove("active-content");
+  content.classList.remove("active-content");
   })
   document.getElementById(id).classList.add("active-content");
+}
 })
+// const span = document.querySelector(".span");
+// const year = new Date().getFullYear();
+// span.textContent = `Copy right ${year}`
